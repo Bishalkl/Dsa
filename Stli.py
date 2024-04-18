981a5f183fa6b5b9a213bf52755bc854ba474a34
Bishal koirala <bishal123koirala@gmail.com>
2024-04-18 16:22:34 +0545

    done

---
+++ Stli.py
@@ -0,0 +1,47 @@
+
+# creating a class for stack by using list() of python
+class Stack:
+    # constructor
+    def __init__(self):
+        self.list = []
+
+    #method for is_empty 
+    def is_empty(self):
+        return self.list is None
+    
+    #method for push:
+    def push(self,data):
+        self.list.append(data)
+
+    #method for pop
+    def pop(self):
+        if self.is_empty():
+            print("Your stack is empty")
+            return None
+        return self.list.pop()
+
+        
+    #method for peek
+    def peek(self):
+        if self.is_empty():
+            print("Stack is empty")
+            return None
+        return self.list[-1]
+    
+
+    def display(self):
+        if self.is_empty():
+            print("List is empty")
+            return
+        for i in range(0, len(self.list)):
+            print(self.list[i], end=" ")
+        print()
+
+        
+#test code
+mylist = Stack()
+mylist.push(23)
+mylist.push(45)
+mylist.push(67)
+mylist.pop()
+mylist.display()
