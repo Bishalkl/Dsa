be1117af1d8f3b8cf4b7ed3c4a14313a2f1f8577
Bishal koirala <bishal123koirala@gmail.com>
2024-05-03 18:20:04 +0545

    done

--- Recursive2.py
+++ Recursive2.py
@@ -14,21 +14,14 @@ print()
 def sumoddN(n):
     if n == 1:
         return 1
-    if not n % 2 == 0:
-        return n + sumoddN(n-1)
-    else:
-        return sumoddN(n-1)
-print(sumoddN(4))
-
+    return 2*n-1 + sumoddN(n-1)
+print(sumoddN(10))
 
 #wap a recursive function to calculate sum of first N eve natural numbers
 def sumevenN(n):
     if n == 1:
-        return 0
-    if n % 2 == 0:
-        return n + sumevenN(n-1)
-    else:
-        return sumevenN(n-1)    
+        return 2
+    return 2*n + sumevenN(n-1) 
 print(sumevenN(4))
 
 #wap a recursive function to calculate a factorial of number.
@@ -38,10 +31,9 @@ def factorialN(n):
     return n * factorialN(n-1)
 print(factorialN(3))
 
-
 #Wap a recursive function to calcuate sum of square of first N natural number
 def sumsquareN(n):
     if n == 1:
         return 1
     return  (n ** 2 ) + sumsquareN(n - 1)
-print(sumsquareN(3))
+print(sumsquareN(5))
