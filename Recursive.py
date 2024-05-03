a3b89aecfcc76d83e64bbb613f6ae51c015d1dd7
Bishal koirala <bishal123koirala@gmail.com>
2024-05-03 17:26:53 +0545

    done

--- Recursive.py
+++ Recursive.py
@@ -16,3 +16,46 @@ print(a)
 
 #Five first basic warm up example 
 
+#First example 
+
+#Wap a recursive function to print first N natural number
+def printN(n):
+    if n >= 1:
+        printN(n-1)
+        print(n, end=" ")
+
+printN(12)
+print()
+
+#Second example 
+#Wap a recursive function to print first N nautral number 
+def printrevN(n):
+    if n >= 1:
+        print(n, end=" ")
+        printrevN(n-1)
+
+printrevN(12)
+print()
+
+
+#Third example 
+#Wap a recursive function to print first N odd natural number
+def printoddN(n):
+    if n >= 1:
+        printoddN(n-1)
+        if not n % 2 == 0:
+            print(n, end=" ")
+
+printoddN(12)
+print()
+
+#Wap a recursive function to print first N even natural number
+def printevenN(n):
+    if n >= 1:
+        printevenN(n - 1)
+        if n % 2 is 0:
+            print(n, end=" ")
+
+printevenN(12)
+print()
+
